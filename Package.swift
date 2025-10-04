5939c04e92834ed748af88a5453502509dd39b0c
hackiftekhar <hack.iftekhar@gmail.com>
2025-10-05 01:25:47 +0530

    - Updated Bundle Id

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbar.git", from: "1.1.1"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbar.git", from: "1.1.2"),
     ],
     targets: [
         .target(name: "IQTextView",
